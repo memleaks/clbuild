d681f9e25e8634d6bb270f67f8a20dbb586d95ac
David Lichteblau <david@lichteblau.com>
2014-08-19 11:31:09 +0300

    CMUCL core saving

--- internal/impl-cmu.sh
+++ internal/impl-cmu.sh
@@ -25,3 +25,9 @@ _configure_implementation() {
     build_options="$noinform -batch $common_options"
     run_options="-batch $common_options"
 }
+
+_print_core_option() {
+    core_name=$1
+    echo '(ext:save-lisp "'$1'")'
+}
+
